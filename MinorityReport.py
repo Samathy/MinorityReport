f5ef66e6253a1afc3c6de7c66bfba510738b4f4c
Sam Barratt <samathy@sbarratt.co.uk>
2016-01-02 21:06:32 +0000

    Added the file that will run all the other files.

---
+++ MinorityReport.py
@@ -0,0 +1,12 @@
+print("This is Minority Report. I will start by importing your bugs into Neo4J.")
+
+import csv_to_Neo4J
+
+print("Now importing Git data")
+
+#import Git2Neo     #Need to make git2neo a proper module first, then I can just import the directory its cloned into.
+#git2neo.main()
+
+
+
+
